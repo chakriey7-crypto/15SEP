04a852576f5d0ff20b1963e40fc4ee4053c98cf0
chakriey7-crypto <chakriey7@gmail.com>
2025-09-15 12:33:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let dog={
+"name":"Gold",Weight:20,color:"yellow"
+
+}
+console.log(typeof(dog))
+console.log(dog)
+dog.breed="Golden retriever"
+dog.name="Gold.D.Roger"
+console.log(dog)
+dog.height=5.6
+console.log(dog)
+console.log(typeof(dog))
